7f4f6076980109016a333dbf22b28d370aaa2a45
Anders Hafreager <anders@tapcat.is>
2014-03-22 16:39:01 +0100

    Added code to create a cylinder shell of thickness r_cut to reduce computation power needed to compute flow in cylinder

--- base_code/md_geometry.py
+++ base_code/md_geometry.py
@@ -8,15 +8,16 @@ class MDGeometry:
 		self.unit_converter = MDUnitConverter(md)
 		self.md_statistics = MDStatistics(md, self.unit_converter)
 	
-	def create_cylinders(self, radius = 0.45, num_cylinders_per_dimension = 1):
+	def create_cylinder(self, radius = 0.45, remove_atoms_outside_outer_shell = False):
 		system_length = self.md_statistics.calculate_system_length()
 		num_nodes = self.md.nodes_x*self.md.nodes_y*self.md.nodes_z
 		max_length = max(system_length[0], system_length[1])
 		print "System length: ", system_length
 		
 		radius = radius*max_length
+		outer_radius = radius+self.md.r_cut
 		self.md.run_command("%s -O3 program/create_cylinder/create_cylinder.cpp -o create_cylinder" % self.md.compiler)
-		self.md.run_command("./create_cylinder "+str(num_nodes)+" "+str(radius)+" "+str(num_cylinders_per_dimension)+" "+str(system_length[0])+" "+str(system_length[1])+" "+str(system_length[2]))
-		volume = system_length[2]*num_cylinders_per_dimension**2*pi*radius**2
+		self.md.run_command("./create_cylinder "+str(num_nodes)+" "+str(radius)+str(system_length[0])+" "+str(system_length[1])+" "+str(system_length[2])+" "+str(int(remove_atoms_outside_outer_shell))+" "+str(outer_radius))
+		volume = system_length[2]*pi*radius**2
 
 		self.md_statistics.update_new_volume(volume=volume)
--- base_code/program/create_cylinder/create_cylinder.cpp
+++ base_code/program/create_cylinder/create_cylinder.cpp
@@ -10,35 +10,43 @@
 using namespace std;
 
 int main(int args, char *argv[]) {
-	if(args < 6) {
-		cout << "Please specify the number of cpus, radius, number of cylinders per dimension, lx, ly" << endl;
+	if(args < 5) {
+		cout << "Please specify the number of cpus, radius, lx, ly" << endl;
 		cout << "./create_cylinder int double int double double" << endl;
 		return 0;
 	}
-	cout << "Will create cylinders." << endl;
-
 	int cpus = atoi(argv[1]);
 	double radius = atof(argv[2]);
-	int num_cylinders_per_dimension = atoi(argv[3]);
 	double system_length[2];
-	system_length[0] = atof(argv[4]);
-	system_length[1] = atof(argv[5]);
-	int num_cylinders = num_cylinders_per_dimension*num_cylinders_per_dimension;
-
-	cout << "Will create " << num_cylinders << " cylinders with radii " << radius << " on a system of size (" << system_length[0] << "," << system_length[1] << ")" << endl;
+	system_length[0] = atof(argv[3]);
+	system_length[1] = atof(argv[4]);
+	bool remove_atoms_outside_shell = false;
 	
+	double outer_radius = radius;
+	if(args > 5) remove_atoms_outside_shell = atoi(argv[5]);
+	if(args > 6) outer_radius = atof(argv[6]);
+	cout << "Will create cylinder with" << endl;
+	cout << "  radius      : " << radius << endl;
+	cout << "  outer radius: " << outer_radius << endl;
+	cout << "  system size : (" << system_length[0] << "," << system_length[1] << ")" << endl;
+	
+	char *filename = new char[100];
 	double *data = new double[6*MAX_ATOM_NUM];
 	unsigned long *atom_type= new unsigned long[MAX_ATOM_NUM];
 	unsigned long *atom_ids = new unsigned long[MAX_ATOM_NUM];
-	char *filename = new char[100];
 	unsigned long num_particles;
 	
 	double radius_squared = radius*radius;
-	int frozen_atoms = 0;
-	int total_num_atoms = 0;
+	double outer_radius_squared = outer_radius*outer_radius;
 
-	double cylinder_center_displacement_x = system_length[0] / num_cylinders_per_dimension;
-	double cylinder_center_displacement_y = system_length[1] / num_cylinders_per_dimension;
+	int num_atoms_before = 0;
+	int num_free_atoms_global = 0;
+	int num_frozen_atoms_global = 0;
+	int num_atoms_global = 0;
+	int num_removed_atoms_global = 0;
+	
+	double cylinder_center_displacement_x = system_length[0];
+	double cylinder_center_displacement_y = system_length[1];
 	
 	for(int cpu=0;cpu<cpus;cpu++) { 
 		sprintf(filename,"state_files/state%04d.bin",cpu);
@@ -48,52 +56,67 @@ int main(int args, char *argv[]) {
 		state_file.read(reinterpret_cast<char*>(data),6*num_particles*sizeof(double));
 		state_file.read(reinterpret_cast<char*>(atom_type),num_particles*sizeof(unsigned long));
 		state_file.read(reinterpret_cast<char*>(atom_ids),num_particles*sizeof(unsigned long));
-		
+		unsigned long num_atoms_local = 0;
+		unsigned long num_free_atoms_local = 0;
+		unsigned long num_frozen_atoms_local = 0;
+		unsigned long num_removed_atoms_local = 0;
 		for(int i=0;i<num_particles;i++) {
+			num_atoms_before++;
+
 			double x = data[6*i+0];
 			double y = data[6*i+1];
-			bool freed_this_atom = false;
-			
-			atom_type[i] = FROZEN;
-			frozen_atoms++;
-			total_num_atoms++;
+			bool removed_this_atom = false;
 
-			for(int cylinder_x=0; cylinder_x<num_cylinders_per_dimension; cylinder_x++) {
-                for(int cylinder_y=0; cylinder_y<num_cylinders_per_dimension; cylinder_y++) {
-                    double cylinder_center_x = cylinder_center_displacement_x*(cylinder_x + 0.5);
-                    double cylinder_center_y = cylinder_center_displacement_y*(cylinder_y + 0.5);
-                    double dx = (x - cylinder_center_x);
-                    double dy = (y - cylinder_center_y);
-                    double dr2 = dx*dx + dy*dy;
+			double cylinder_center_x = cylinder_center_displacement_x*0.5;
+            double cylinder_center_y = cylinder_center_displacement_y*0.5;
+            double dx = (x - cylinder_center_x);
+            double dy = (y - cylinder_center_y);
+            double dr2 = dx*dx + dy*dy;
 
-                    if(dr2 < radius_squared) {
-                        atom_type[i] = ARGON;
-                        frozen_atoms--;
-                        freed_this_atom = true;
-                    }
+            if(dr2 < radius_squared) {
+                atom_type[i] = ARGON;
 
-                    if(freed_this_atom) break;
-                }
+                num_free_atoms_local++;
+            } else if(dr2 < outer_radius_squared || !remove_atoms_outside_shell) {
+            	atom_type[i] = FROZEN;
+            	num_frozen_atoms_local++;
+            } else if(remove_atoms_outside_shell) {
+            	removed_this_atom = true;
+            	num_removed_atoms_local++;
+            }
 
-                if(freed_this_atom) break;
+            if(!removed_this_atom) {
+            	data[6*num_atoms_local + 0] = data[6*i+0];
+            	data[6*num_atoms_local + 1] = data[6*i+1];
+            	data[6*num_atoms_local + 2] = data[6*i+2];
+            	data[6*num_atoms_local + 3] = data[6*i+3];
+            	data[6*num_atoms_local + 4] = data[6*i+4];
+            	data[6*num_atoms_local + 5] = data[6*i+5];
+            	atom_type[num_atoms_local] = atom_type[i];
+            	atom_ids[num_atoms_local] = atom_ids[i];
+            	num_atoms_local++;
             }
 		}
 		state_file.close();
 
+		num_atoms_global += num_atoms_local;
+		num_frozen_atoms_global += num_frozen_atoms_local;
+		num_free_atoms_global += num_free_atoms_local;
+		num_removed_atoms_global += num_removed_atoms_local;
+
 		ofstream save_state_file(filename,ios::out | ios::binary);
-		save_state_file.write(reinterpret_cast<char*>(&num_particles),sizeof(unsigned long));
-		save_state_file.write(reinterpret_cast<char*>(data),6*num_particles*sizeof(double));
-		save_state_file.write(reinterpret_cast<char*>(atom_type),num_particles*sizeof(unsigned long));
-		save_state_file.write(reinterpret_cast<char*>(atom_ids),num_particles*sizeof(unsigned long));
+		save_state_file.write(reinterpret_cast<char*>(&num_atoms_local),sizeof(unsigned long));
+		save_state_file.write(reinterpret_cast<char*>(data),6*num_atoms_local*sizeof(double));
+		save_state_file.write(reinterpret_cast<char*>(atom_type),num_atoms_local*sizeof(unsigned long));
+		save_state_file.write(reinterpret_cast<char*>(atom_ids),num_atoms_local*sizeof(unsigned long));
 		save_state_file.close();
 	}
 
-	int free_atoms = total_num_atoms - frozen_atoms;
-
 	// Write the number of free atoms
-	cout << num_cylinders << " cylinders created with " << frozen_atoms << " frozen atoms and " << free_atoms << " free atoms." << endl;
+	cout << "Createad cylinder with " << num_frozen_atoms_global << " frozen atoms, " << num_free_atoms_global << " free atoms and " << num_removed_atoms_global << " removed atoms." << endl;
+	cout << "We had " << num_atoms_before << " atoms before this process. " << endl;
 	ofstream free_atoms_file("number_of_free_atoms.txt");
-	free_atoms_file << free_atoms;
+	free_atoms_file << num_free_atoms_global;
 	free_atoms_file.close();
 
 	return 0;
